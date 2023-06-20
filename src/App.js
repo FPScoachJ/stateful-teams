aea4ac2d79e3264567bf73c978e7de7a92c5f4df
Justin Anzalone <justin.m.anzalone@gmail.com>
2023-06-20 07:53:43 -0700

    Fixed Dependencies and cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import MainContainer from "./components/MainContainer";
+import TextField from "@mui/material/TextField";
 import { useState } from "react";
 import dustin from "./assets/dustin.png";
 import daniel from "./assets/daniel.jpeg";
@@ -13,20 +14,49 @@ function App() {
     { name: "max", img: max },
     { name: "justin", img: justin },
   ];
-  const [students, setStudents] = useState(studentData);
+  const addTeams = studentData.map((student) => ({
+    ...student,
+    team: "default",
+  }));
+  const [students, setStudents] = useState(addTeams);
+  const [binarybots, setBinarybots] = useState([]);
+  const [bccrew, setBccrew] = useState([]);
+
+  const moveStudentToTeam = (student, teamName) => {
+    if (teamName === "binarybots") {
+      setBinarybots([...binarybots, { ...student, team: "binarybots" }]);
+      setBccrew((state) => state.filter((e) => e.name !== student.name));
+      setStudents((state) => state.filter((e) => e.name !== student.name));
+    } else {
+      setBccrew([...bccrew, { ...student, team: "bccrew" }]);
+      setBinarybots((state) => state.filter((e) => e.name !== student.name));
+      setStudents((state) => state.filter((e) => e.name !== student.name));
+    }
+  };
+
+  const reset = () => {
+    setBinarybots([]);
+    setBccrew([]);
+    setStudents(addTeams);
+  };
+
   return (
     <div className="topcontainer">
       <h1>Student Team React Challenge</h1>
-      <button
-        className="button-24"
-        onClick={() =>
-          console.log("I should reset you back to all being in default")
-        }
-      >
+      <button className="button-24" onClick={reset}>
         Reset
       </button>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
       <div className="App">
-        <MainContainer students={students} />
+        <MainContainer
+          students={students}
+          binarybots={binarybots}
+          setBinarybots={setBinarybots}
+          bccrew={bccrew}
+          setStudents={setStudents}
+          setBccrew={setBccrew}
+          moveStudentToTeam={moveStudentToTeam}
+        />
       </div>
     </div>
   );
--- src/components/Student.js
+++ src/components/Student.js
@@ -1,13 +1,63 @@
 import React from "react";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
 
-export default function Student({ student }) {
+export default function Student({
+  student,
+  students,
+  setBinarybots,
+  binarybots,
+  moveStudentToTeam,
+}) {
   return (
     <div className="studentCard">
       <h3>{student.name}</h3>
-      <img className="student-pic" src={student.img} alt="" />
+      <Avatar
+        className="student-pic"
+        src={student.img}
+        alt=""
+        sx={{ width: 130, height: 130 }}
+      />
       <div>
-        <button className="button-24">Binary Bots</button>
-        <button className="button-24">BC Crew</button>
+        {/* {if this thing is true, do this code otherwise return null} */}
+        {student?.team === "default" && (
+          <>
+            <Button
+              name="bccrew"
+              onClick={(e) => moveStudentToTeam(student, e.target.name)}
+              variant="contained"
+            >
+              BC Crew
+            </Button>
+
+            <Button
+              onClick={(e) => moveStudentToTeam(student, e.target.name)}
+              color="success"
+              variant="contained"
+              name="binarybots"
+            >
+              Binary Bots
+            </Button>
+          </>
+        )}
+        {student?.team === "bccrew" && (
+          <Button
+            onClick={(e) => moveStudentToTeam(student, e.target.name)}
+            name="binarybots"
+            variant="contained"
+          >
+            Binary Bots
+          </Button>
+        )}
+        {student?.team === "binarybots" && (
+          <Button
+            name="bccrew"
+            variant="contained"
+            onClick={(e) => moveStudentToTeam(student, e.target.name)}
+          >
+            BC Crew
+          </Button>
+        )}
       </div>
     </div>
   );
